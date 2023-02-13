6bbabdf5c3a7097c3a55486eb0c43a5cb3cb401a
Brook-A-CS <brooka048@gmail.com>
2023-02-13 17:05:44 -0600

    updated Selenium syntax

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import logging
 import pathlib
 
 parent_directory = pathlib.Path(__file__).parent.absolute()
-PATH = f'{parent_directory}\chromedriver.exe'
+PATH = 'C:\Program Files\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://public.txdpsscheduler.com/")
 
 def convert_to_datetime(x):
     
-    dateX = driver.find_element_by_xpath(x)
+    dateX = driver.find_element("xpath", x)
     date = dateX.text
 
     seperators = [i.start() for i in re.finditer("/", date)]
@@ -38,7 +38,7 @@ def convert_to_datetime(x):
 
 def check_exist(xpath):
     try:
-      exists = driver.find_element_by_xpath(xpath)
+      exists = driver.find_element("xpath", xpath)
     except NoSuchElementException:
         return False
     return exists
@@ -47,71 +47,75 @@ first_name = "FirstName"
 last_name = "LastName"
 birth_digits = "01012001"
 email = "insertEmail@gmail.com"
-zip_code = "99999"
+zip_code = "75002"
 ssn = "1234"
 
 existingAppointment = False
-email_input_id = 138
-conf_email_id = 141
-zip_code_input_id = 164
+email_input_id = 138 - 4
+conf_email_id = 141 - 4
+zip_code_input_id = 164 - 4
 
 
-lang = driver.find_element_by_xpath('//*[@id="app"]/div[2]/div/div/div[2]/button[1]/span')
+lang = driver.find_element("xpath", '//*[@id="app"]/div[2]/div/div/div[2]/button[1]/span')
+
 lang.click()
 
-F_N = driver.find_element_by_id("input-55")
+F_N = driver.find_element("id", "input-55")
 F_N.send_keys(first_name)
 
-L_N = driver.find_element_by_id("input-58")
+L_N = driver.find_element("id", "input-58")
 L_N.send_keys(last_name)
 
-DOB = driver.find_element_by_id("dob")
+DOB = driver.find_element("id", "dob")
 DOB.send_keys(birth_digits)
 
-SSN = driver.find_element_by_id("last4Ssn")
+SSN = driver.find_element("id", "last4Ssn")
 SSN.send_keys(ssn)
 
 driver.implicitly_wait(10) 
 
 
-log_on = driver.find_element_by_xpath('//*[@id="app"]/section/div/main/div/section/div[2]/div/div/form/div[2]/div[4]/button/span') 
+log_on = driver.find_element("xpath", '//*[@id="app"]/section/div/main/div/section/div[2]/div/div/form/div[2]/div[4]/button/span') 
 log_on.click()
 
 driver.implicitly_wait(10) 
-new_appointment = driver.find_element_by_xpath('//*[@id="app"]/section/div/main/div/section/div[2]/div/div/div[3]/div/button')
+new_appointment = driver.find_element("xpath", '//*[@id="app"]/section/div/main/div/section/div[2]/div/div/div[3]/div/button')
 new_appointment.click()
 
-if check_exist('//*[@id="app"]/div[1]/div/div/div[2]/button/span'):
-    existingAppointment = True
+# if check_exist('//*[@id="app"]/div[1]/div/div/div[2]/button/span'):
+#     existingAppointment = True
 
-if existingAppointment:
-    appointment_exists = driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[2]/button/span')
-    appointment_exists.click()
-elif not existingAppointment: 
-    email_input_id -= 4
-    conf_email_id -= 4
-    zip_code_input_id -= 4 
-    pass
-else:
-    print("error, cannot detrmine xpath eligibility")
+existingAppointment = True
+
+# if existingAppointment:
+#     appointment_exists = driver.find_element("xpath", '//*[@id="app"]/section/div/main/div/section/div[2]/div/main/div/div/div[1]/div[1]/button')
+#     appointment_exists.click()
+# elif not existingAppointment: 
+#     email_input_id -= 4
+#     conf_email_id -= 4
+#     zip_code_input_id -= 4 
+#     pass
+# else:
+#     print("error, cannot detrmine xpath eligibility")
 
 
-apply_permit = driver.find_element_by_xpath('//*[@id="app"]/section/div/main/div/section/div[2]/div/main/div/div/div[1]/div[1]/button')
+
+apply_permit = driver.find_element("xpath", '//*[@id="app"]/section/div/main/div/section/div[2]/div/main/div/div/div[1]/div[1]/button')
 apply_permit.click()
 
-email_input = driver.find_element_by_id(f"input-{email_input_id}")
+email_input = driver.find_element("id", f"input-{email_input_id}")
 email_input.send_keys(email)
 
-conf_email = driver.find_element_by_id(f"input-{conf_email_id}")
+conf_email = driver.find_element("id", f"input-{conf_email_id}")
 conf_email.send_keys(email)
 
-zip_code_input = driver.find_element_by_id(f"input-{zip_code_input_id}")
+zip_code_input = driver.find_element("id", f"input-{zip_code_input_id}")
 zip_code_input.send_keys(zip_code)
 
 
 while True:
 
-    next = driver.find_element_by_xpath('//*[@id="app"]/section/div/main/div/section/div[2]/div/form/div/div[2]/div[2]/div/div[2]/button/span')
+    next = driver.find_element("xpath", '//*[@id="app"]/section/div/main/div/section/div[2]/div/form/div/div[2]/div[2]/div/div[2]/button/span')
     next.click()
 
     new_Second = convert_to_datetime('//*[@id="app"]/section/div/main/div/section/div[2]/div/div[2]/div/table/tbody/tr[1]/td[3]')
@@ -119,7 +123,7 @@ while True:
     new_Closest = convert_to_datetime('//*[@id="app"]/section/div/main/div/section/div[2]/div/div[1]/div/table/tbody/tr/td[3]')
 
     #format: (year, month, day)
-    current = datetime.date(2022, 7, 26)
+    current = datetime.date(2024, 1, 31)
 
     print(f'Closest: {new_Closest} SecondClosest: {new_Second}')
 
@@ -128,8 +132,8 @@ while True:
         duration = 2500  # milliseconds
         freq = 440  # Hz
         winsound.Beep(freq, duration)
-        break    
+        time.sleep(999)    
     else:
-        time.sleep(3)
-        previous = driver.find_element_by_xpath('//*[@id="app"]/section/div/main/div/section/div[2]/div/div[5]/div/div[1]/button/span/i')
+        time.sleep(1)
+        previous = driver.find_element("xpath", '//*[@id="app"]/section/div/main/div/section/div[2]/div/div[5]/div/div[1]/button/span/i')
         previous.click()
